fb30a0f897e047c471f8c649440de1f2dac8b1d8
aditya21891 <prattiaditya@gmail.com>
2017-04-10 21:47:44 -0700

    New program added

---
+++ learna.py
@@ -0,0 +1,10 @@
+print "I will now count my chickens "
+print "hens", 25 + 30/6
+print "Roosters", 100 - 25*3%4
+print "Now i will count the eggs"
+print 3 + 2 + 1 -5 + 4%2 - 1/4 + 6
+print "Is it true that 3+2 < 5- 7 ?"
+print 3+2 < 5- 7
+print " what is: ", 999 + 343
+print "how much is", 80007-98235
+print " which 1 is greater or lesser"
