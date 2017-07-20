cf514175e0c3106bba53464ec0bd4e937e5726b8
aditya21891 <prattiaditya@gmail.com>
2017-07-20 18:16:19 -0500

    bored changed

--- bored.py
+++ bored.py
@@ -2,6 +2,8 @@
 print("Welcome to Python")
 print ("Today we are going to write some python code")
 user1=raw_input("Enter your name: ")
-userag=raw_input("Enter your age: ")
-while userag == 34 :
-    print"  Are you planning for retirement ? "
+userag=int(input("Enter your age: "))
+if userag >= 34 :
+    print"Are you planning for retirement?"
+else:
+	print "you have to work hard for your goal"
