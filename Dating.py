60e9ad8d72d2e6673bb10782f163dfbb9881ee14
aditya21891 <prattiaditya@gmail.com>
2017-07-20 18:09:51 -0500

    Date file changed

--- Dating.py
+++ Dating.py
@@ -1,6 +1,6 @@
 # program to find the dating age
-nam=raw_input("Enter your name: ")
-age=raw_input("Enter your age : ")
+name=raw_input("Enter your name: ")
+age=int(input("Enter your age : "))
 if (age >= 21):
   print "Ready to date "
 else:
