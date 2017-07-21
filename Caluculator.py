3f2f65be30bef559016df3c3cf3aa1ba41472bf0
aditya21891 <prattiaditya@gmail.com>
2017-07-21 09:29:23 -0500

    Modify calc.py

--- Caluculator.py
+++ Caluculator.py
@@ -1,21 +1,21 @@
-# a program to Caluculate values
+# a calc program for 2 numbers 
 print("Welcome to  my calc")
 typeofop=raw_input("Would you like to add(a), substract(s), Multiply(m), Divison(d) ? ")
 if typeofop == "a":
-    x=int(input("Enter the first number: "))
-    y=int(input("Enter the second number:"))
+    x=int(raw_input("Enter the first number: "))
+    y=int(raw_input("Enter the second number:"))
     print x + y
 elif typeofop == "s":
-    x=int(input("Enter the first number: "))
-    y=int(input("Enter the second number:"))
+    x=int(raw_input("Enter the first number: "))
+    y=int(raw_input("Enter the second number:"))
     print x - y
 elif typeofop == "m":
-    x=int(input("Enter the first number: "))
-    y=int(input("Enter the second number:"))
-    print x * y
+   x=int(raw_input("Enter the first number: "))
+   y=int(raw_input("Enter the second number:"))
+   print x * y
 elif typeofop == "d":
-    x=int(input("Enter the first number: "))
-    y=int(input("Enter the second number:"))
+    x=int(raw_input("Enter the first number: "))
+    y=int(raw_input("Enter the second number:"))
     print x / y
 else :
     print("Wrong input" + typeofop)
