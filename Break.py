3a1119e5bcbfaf5ecf2cc4f13c717dd8856ea078
aditya21891 <prattiaditya@gmail.com>
2017-07-23 12:37:12 -0500

    Break

---
+++ Break.py
@@ -0,0 +1,6 @@
+#A program which indicate me take a break for every 2 hours from system 
+import webbrowser 
+import time 
+
+time.sleep(60)
+webbrowser.open("https://www.youtube.com/watch?v=jIqRbFQl-ds")
