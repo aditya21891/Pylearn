ab034871f4a9bc2a70566d522797130ebf40af57
Aditya Pratti <prattiaditya@gmail.com>
2017-08-03 15:13:25 -0500

    New files

---
+++ BMRrate.py
@@ -0,0 +1,7 @@
+# a python program to fing the BMR rate 
+wght=input("Please enter your weight in pounds:")
+hght=input("Enter your height in inch:")
+age=input("Enter your age: ")
+bmr=66.5 + (6.23*wght) + (12.7*hght) - (6.8*age)
+print bmr 
+
