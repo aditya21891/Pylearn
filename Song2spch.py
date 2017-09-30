cc01de487a05561c5829d571792dde9ae79090ae
Aditya Pratti <prattiaditya@gmail.com>
2017-09-29 23:11:01 -0700

    Added changes

--- Song2spch.py
+++ Song2spch.py
@@ -1,5 +1,5 @@
 from gtts import gTTS
 import os 
-tts=gTTS(text="Hello Anoop Have a good day all the best ",lang='en')
+tts=gTTS(text="Hello Aditya Have a good day all the best ",lang='en')
 tts.save("hello.mp3")
 os.system("mpg321 hello.mp3")
